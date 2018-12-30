2ea4cf4f6275028605aa3d2ea8a8e51e91fe43b8
newbalancem5 <newbalancem5@gmail.com>
2018-12-30 20:10:21 +0300

    add new icon for music

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -11,8 +11,6 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import MusicNote from '@material-ui/icons/MusicNote'; 
 
 const drawerWidth = 240;
@@ -69,7 +67,7 @@ function PermanentDrawerLeft(props) {
         <List>
           {['Music', 'YourSongs', 'Music Every Day', 'Shaffle'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <MusicNoteIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemIcon>{index % 2 === 0 ? <MusicNote /> : <MusicNote />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
