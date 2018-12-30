2a4a1985d9c80492cd87a352b6cf65ad1444d45b
newbalancem5 <newbalancem5@gmail.com>
2018-12-30 19:25:57 +0300

    add menu

--- public/Router.js
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-import {Route, Indexroute} from 'react-router'; 
--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Contacts from './Components/Contacts/Contacts';
-import News from './Components/News/News';
-import Song from './Components/Song/Song';
+import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import Menu from './Components/Menu/Menu';
 
-
-
-
-function AppMenu(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            ReactWithElectronMusic
-          </Typography>
-          
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+   <BrowserRouter>
+      <Menu/>
+   </BrowserRouter> 
+    );
+  }
 }
 
-AppMenu.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
-
-export default withStyles(styles)(AppMenu);
+export default App;
